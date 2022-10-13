6b0490429ce0c2c524ff1b17d2b61f42d2ab93eb
rolwane-meli <francisco.rolwane@mercadolivre.com>
2022-10-13 14:42:28 -0300

    initial commit

---
+++ src/main/java/br/com/bootcamp/desafio_testing/DesafioTestingApplication.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.desafio_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bootcamp/desafio_testing/DesafioTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.desafio_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
