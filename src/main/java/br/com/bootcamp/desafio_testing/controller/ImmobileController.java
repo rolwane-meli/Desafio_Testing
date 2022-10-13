fc7d78b657f7dcaaa54697b4b6786a682f0c5617
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-10-13 17:55:13 -0300

    feat: endpoint calcula area total

--- src/main/java/br/com/bootcamp/desafio_testing/controller/ImmobileController.java
+++ src/main/java/br/com/bootcamp/desafio_testing/controller/ImmobileController.java
@@ -1,10 +1,27 @@
 package br.com.bootcamp.desafio_testing.controller;
 
+import br.com.bootcamp.desafio_testing.dto.ImmobileDTO;
+import br.com.bootcamp.desafio_testing.service.ImmobileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 @RequestMapping("/api/v1/immobile")
 public class ImmobileController {
 
+    @Autowired
+    ImmobileService service;
+
+    @GetMapping("/area/{immobileName}")
+    public ResponseEntity<ImmobileDTO> getImmobile(@PathVariable String immobileName) throws ClassNotFoundException {
+        ImmobileDTO immobile = service.getImmobile(immobileName);
+        return  new ResponseEntity<ImmobileDTO>(immobile, HttpStatus.OK);
+    }
 }
